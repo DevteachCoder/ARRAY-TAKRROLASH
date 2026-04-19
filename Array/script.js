3535b3d3aaa0ca38293ee64067c5c77c3707c39b
Doniyorbek <142497625+DevteachCoder@users.noreply.github.com>
2026-04-19 23:59:37 +0500

    Add files via upload

---
+++ Array/script.js
@@ -0,0 +1,34 @@
+// let name = "Javohir";
+// let age = 22;
+// let email = "user@.com";
+
+// let user = {
+//     name:"Javohir",
+//     age:22,
+//     email:"user@.com"
+// };
+
+// user.email = "Berdiyeb@.ru";
+// user.color = "blue"
+
+// console.log(user);
+
+
+// let a = [4,5,6,7,8,3,2,54,78,65,444,34,98];
+
+// let k = a.length;
+
+
+// let arr = ['Apple',{name:'John'},true,function() {alert('hello');}];
+
+// alert(arr[1]);
+
+// pop orqasidan o'chiradi
+// push orqasidan qo'shadi
+// shift boshidan qo;shadi
+
+  let fruits = ['orange','apple','grape','banana'];
+
+  fruits.push('kive');
+  console.log(fruits); 
+
